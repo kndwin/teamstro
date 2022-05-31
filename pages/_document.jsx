0366a937932678a6f37b9cf37020566fbf5ee7cb
Kevin Nguyen <me@kndwin.dev>
2022-05-31 13:46:10 +1000

    fix: update correct document

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,24 +1,14 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ServerStyles, createStylesServer } from "@mantine/next";
+import { createGetInitialProps } from '@mantine/next';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+const getInitialProps = createGetInitialProps();
 
 export default class _Document extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          <ServerStyles html={initialProps.html} server={stylesServer} />
-        </>
-      ),
-    };
-  }
+  static getInitialProps = getInitialProps;
 
   render() {
     return (
-      <Html lang={this.props.locale}>
+      <Html>
         <Head />
         <body>
           <Main />

9f0b669e5ff5cf1fca2cb331d477d9a118fb0f51
Kevin Nguyen <me@kndwin.dev>
2022-05-31 13:36:22 +1000

    fix: support RTL for Mantine (removes FOUC)

---
+++ pages/_document.jsx
@@ -0,0 +1,35 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import rtlPlugin from "stylis-plugin-rtl";
+import { ServerStyles, createStylesServer } from "@mantine/next";
+
+export default class _Document extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const stylesServer = createStylesServer({
+      key: "rtl",
+      stylisPlugins: [rtlPlugin],
+    });
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <ServerStyles html={initialProps.html} server={stylesServer} />
+        </>
+      ),
+    };
+  }
+
+  render() {
+    return (
+      <Html dir="rtl" lang={this.props.locale}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
